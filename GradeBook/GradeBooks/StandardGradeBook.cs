a3a33134ed608659983d36f79bb57455914f0bb4
josepiratilla <jose.huerta@sunhotels.com>
2019-12-15 22:51:37 +0100

    Hola

---
+++ GradeBook/GradeBooks/StandardGradeBook.cs
@@ -0,0 +1,11 @@
+namespace GradeBook.GradeBooks
+{
+    public class StandardGradeBook : BaseGradeBook
+    {
+        public StandardGradeBook(string name) : base(name)
+        {
+            
+        }
+
+    }
+}
